efd3b57e8183fa6a5e2e450cf3abb974f0316c54
vansh Sharma <vanshsharma7832@gmail.com>
2026-02-21 15:26:01 +0530

    Added Rate Limit

--- Backend_node/src/app.ts
+++ Backend_node/src/app.ts
@@ -9,6 +9,8 @@ import performanceRouter from "./routes/performance.routes.js"
 import questionRouter from "./routes/question.routes.js";
 import CodingSchemaRouter from "./routes/coding.routes.js"
 import { Request, Response } from "express";
+
+import {rateLimit} from 'express-rate-limit'
 connectdb()
 const app=new Vexpress();
 app.setupDocs();
@@ -19,6 +21,20 @@ app.use(cors({
     ,credentials:true
 }));
 app.use(cookieParser())
+
+// Implementing Token Bucket
+// bucker
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+app.use(limiter)
 
 app.use(Vexpress.urlencoded({extended:true,limit:"16kb"}))// extended true bole to or obj ke under obj de pate h and ye url me jo data hota h usko deta h
 app.use(Vexpress.json({limit:"16kb"})) // asa no ho ki kitne bhi json bhej de
@@ -31,7 +47,8 @@ app.use("/api/performance",performanceRouter);
 app.use("/api/question",questionRouter);
 app.use("/api/codingSchema",CodingSchemaRouter)
 
-app.use("/",(req:Request,res:Response)=>{
+app.use("/",async (req:Request,res:Response)=>{
+    await new Promise(resolve=>setTimeout(resolve,2000))
     res.status(200).json({message:"Hello World"})
 })
 export default app;
