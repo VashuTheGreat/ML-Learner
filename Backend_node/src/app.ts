9f4fa177a71acbae9c4dbd48b3a503284812be56
vansh Sharma <vanshsharma7832@gmail.com>
2026-02-21 15:42:16 +0530

    Added Rate Limiter

--- Backend_node/src/app.ts
+++ Backend_node/src/app.ts
@@ -25,14 +25,24 @@ app.use(cookieParser())
 // Implementing Token Bucket
 // bucker
 
+interface Limiter {
+  windowMs: number;
+  limit: number;
+  standardHeaders: 'draft-6' | 'draft-7' | 'draft-8' | boolean;
+  legacyHeaders: boolean;
+  ipv6Subnet?: number;  // Optional, defaults to 56
+  // store?: RateLimitStore;  // Optional for Redis/MemoryStore
+}
+
+// console.log(process.env)
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-	// store: ... , // Redis, Memcached, etc. See below.
-})
+  windowMs: Number(process.env.windowMs)* 60 * 1000,
+  limit: Number(process.env.limit),
+  standardHeaders: process.env.standardHeaders as 'draft-6' | 'draft-7' | 'draft-8' | boolean,
+  legacyHeaders: process.env.legacyHeaders === 'true',
+  ipv6Subnet: Number(process.env.ipv6Subnet),
+} satisfies Limiter);  
+
 
 app.use(limiter)
 
