e00a57050065fe03f872f0df04e1bc207af089d0
vansh Sharma <vanshsharma7832@gmail.com>
2026-03-21 22:14:06 +0530

    checked backend node working

--- Backend_node/src/app.ts
+++ Backend_node/src/app.ts
@@ -23,7 +23,7 @@ app.setupDocs();
 
 logger.info("Server is starting at http://localhost:8000")
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:5174','http://localhost:8080','*']
+    origin:['http://localhost:5173','http://localhost:5174','http://localhost:8080','*','http://10.226.207.207:8080']
     ,credentials:true
 }));
 app.use(cookieParser())
