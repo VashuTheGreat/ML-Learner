1591b2e253bfd12c7dfbd045f58487232dab833a
vansh Sharma <vanshsharma7832@gmail.com>
2026-03-21 22:24:17 +0530

    checked python_backend working

--- python_backend/main.py
+++ python_backend/main.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    uv.run(app, host="0.0.0.0", port=7836, reload=False)
+    uv.run(app, host="0.0.0.0", port=7860, reload=False)
