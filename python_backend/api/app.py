8e486d369dfac7956778705589ca26e939b748bd
VashuTheGreat <vanshsharma7832@gmail.com>
2026-04-05 16:26:12 +0530

    added cors mlearner.tech

--- python_backend/api/app.py
+++ python_backend/api/app.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*","https://www.mlearner.tech"], 
     allow_credentials=False,
     allow_methods=["*"], 
     allow_headers=["*"],
